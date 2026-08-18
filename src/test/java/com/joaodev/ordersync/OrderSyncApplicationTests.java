2e4e1d43acf92bfa17589ddcdaaf786af10c2fba
João Augusto Bonfante <joao.dev93@gmail.com>
2026-08-18 19:42:44 -0300

    chore: initial Spring Boot 4.1 project setup
    - Java 21, Maven
    - Dependencies: Web, Data JPA, Flyway, Actuator, Validation, Lombok

---
+++ src/test/java/com/joaodev/ordersync/OrderSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.ordersync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
